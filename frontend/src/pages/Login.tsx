7b4f6e713b7d42ad1821638ec3e71e878e4742e8
psu6810110229 <6810110229@psu.ac.th>
2025-12-29 20:42:17 +0700

    FE: update login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import apiClient from '../api/client';
-import { AuthResponse } from '../types';
+import type{ AuthResponse } from '../types';
 import { Lock, User } from 'lucide-react';
 
 const Login = () => {
